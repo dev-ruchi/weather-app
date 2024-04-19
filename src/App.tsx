17024c7537b4562075638ea5ec89e7d90fb73095
Ruchi <ruchi.thanabhawan@gmail.com>
2024-04-18 20:21:23 -0700

    fetch and log cities

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+type Coordinates = {
+  lon: number;
+  lat: number;
+};
+
+type City = {
+  geoname_id: string;
+  name: string;
+  ascii_name: string;
+  alternate_names: string[];
+  feature_class: string;
+  feature_code: string;
+  country_code: string;
+  cou_name_en: string;
+  country_code_2: string | null;
+  admin1_code: string;
+  admin2_code: string;
+  admin3_code: string | null;
+  admin4_code: string | null;
+  population: number;
+  elevation: number | null;
+  dem: number;
+  timezone: string;
+  modification_date: string;
+  label_en: string;
+  coordinates: Coordinates;
+};
+
+type opendatasoftResponse = {
+  total_count: number;
+  results: City[]
+}
+
+
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    fetch(`https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000/records?limit=50`)
+    .then(res => res.json())
+    .then((data: opendatasoftResponse) => {
+      console.log(data.results)
+    })
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
